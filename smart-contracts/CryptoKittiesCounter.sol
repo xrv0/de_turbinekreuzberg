ef757e5a47d37b246e90ec7164a191ce09765565
xrv0 <33809091+xrv0@users.noreply.github.com>
2020-09-30 15:13:07 +0200

    Update CryptoKittiesCounter.sol

--- smart-contracts/CryptoKittiesCounter.sol
+++ smart-contracts/CryptoKittiesCounter.sol
@@ -1,13 +1,19 @@
 pragma solidity ^0.6.0;
 
-abstract contract ERC721 {
-    function balanceOf(address _owner) public virtual view returns (uint256 balance);
-}
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/token/ERC721/IERC721.sol";
 
 contract CryptoKittiesCounter {
-    address constant contractAdress = 0x16baF0dE678E52367adC69fD067E5eDd1D33e3bF; // contractAdess vom cryptokitties contract
+    address public ckAddress;
+
+    constructor() public {
+        ckAddress = 0x16baF0dE678E52367adC69fD067E5eDd1D33e3bF;
+    }
+
+    function setCryptoKittiesAddress(address newCkAddress) public {
+        ckAddress = newCkAddress;
+    }
     
     function getKittieBalance() public view returns (uint256) {
-        return ERC721(contractAdress).balanceOf(address(this));
+        return IERC721(ckAddress).balanceOf(address(this));
     }
 }
