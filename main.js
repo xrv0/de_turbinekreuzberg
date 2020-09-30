385f094d700e97660c174433226071ce0a64afd8
xrv0 <33809091+xrv0@users.noreply.github.com>
2020-09-30 14:59:47 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,14 +1,13 @@
 const provider = new ethers.providers.Web3Provider(window.ethereum)
-provider.getBlockNumber().then(number => {
-    console.log(number)
-});
 
 const contractAddress = "0x6ebcae41064dfeb758c6ae2408c8c1a2656ecb1d"
 const abi = [ { "inputs": [], "name": "getKittieBalance", "outputs": [ { "internalType": "uint256", "name": "", "type": "uint256" } ], "stateMutability": "view", "type": "function" } ];
 const smartContract = new ethers.Contract(contractAddress, abi, provider);
 
-smartContract.getKittieBalance().then(output => {
-    let kittenBalance = parseInt(output._hex, 16)
-    const kittenCounterElement = document.getElementById("kitten-counter");
-    kittenCounterElement.innerText = kittenBalance.toString();
-});
+window.setInterval(() => {
+    smartContract.getKittieBalance().then(output => {
+        let kittiesBalance = parseInt(output._hex, 16)
+        const kittiesCounterElement = document.getElementById("kitties-counter");
+        kittiesCounterElement.innerText = kittenBalance.toString();
+    });
+}, 5* 1000);
