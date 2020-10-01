9e2b5d929ce31d674ac463192550158067903c18
xrv0 <33809091+xrv0@users.noreply.github.com>
2020-10-01 14:58:19 +0200

    Added abilitity to post messages

--- de-pinboard/main.js
+++ de-pinboard/main.js
@@ -1,5 +1,6 @@
-const provider = new ethers.providers.Web3Provider(window.ethereum)
-
+let contract;
+let signer;
+let provider;
 const contractAbi = [
     {
         "inputs": [
@@ -48,36 +49,45 @@ const contractAbi = [
     }
 ];
 const contractAddress = "0x560962D72C77Ad90F5e1839FeF8E8499d46688CB";
-const contract = new ethers.Contract(contractAddress, contractAbi, provider);
 
-let messages = [];
-setInterval(() => {
-    contract.getMessagesSize().then(output => {
-        const messagesList = document.querySelector("#post-list")
+window.ethereum.enable().then(() => {
+    provider = new ethers.providers.Web3Provider(window.ethereum);
+    signer = provider.getSigner();
 
-        while(messagesList.firstChild) {
-            messagesList.removeChild(messagesList.firstChild);
-        }
+    contract = new ethers.Contract(contractAddress, contractAbi, signer);
 
-        let size = parseInt(output._hex, 16);
-        console.log("Found " + size + " messages!");
-        for(let i = 0; i < size; i++) {
-            contract.messages(i).then(message => {
-                messages.push(message);
+    let messages = [];
+    setInterval(() => {
+        contract.getMessagesSize().then(output => {
+            const messagesList = document.querySelector("#post-list")
 
-                let li = document.createElement("li");
-                li.appendChild(document.createTextNode(message));
-                messagesList.appendChild(li);
-            })
-        };
-    });
-}, 10 * 1000);
+            while(messagesList.firstChild) {
+                messagesList.removeChild(messagesList.firstChild);
+            }
 
-const postButton = document.querySelector("#message-submit-button");
-const messageBox = document.querySelector("#message-box");
-postButton.onclick = () => {
-    const text = messageBox.textContent;
+            let size = parseInt(output._hex, 16);
+            console.log("Found " + size + " messages!");
+            for(let i = 0; i < size; i++) {
+                contract.messages(i).then(message => {
+                    messages.push(message);
+
+                    let li = document.createElement("li");
+                    li.appendChild(document.createTextNode(message));
+                    messagesList.appendChild(li);
+                })
+            };
+        });
+    }, 10 * 1000);
+});
+
+const messageBox = document.getElementById("message-box")
+
+function submitMessage() {
+    const text = messageBox.value;
+    console.log(text);
     if(text.length > 0) {
-        contract.postMessage(text);
+        contract.postMessage(text).then(() => {
+            console.log("Posted! Text: " + text);
+        });
     }
 }
