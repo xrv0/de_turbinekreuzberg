673e81ff7f0be752232e450914f25738687cd00f
Stefan Adolf <stadolf@gmail.com>
2020-10-05 17:26:25 +0200

    updated shop code from Mohan

--- cryptokitties-shop/contracts/CryptoKittiesShop.sol
+++ cryptokitties-shop/contracts/CryptoKittiesShop.sol
@@ -2,6 +2,7 @@
 pragma solidity ^0.6.12;
 
 // Import the IERC20 interface and and SafeMath library
+import "@openzeppelin/contracts-ethereum-package/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts-ethereum-package/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts-ethereum-package/contracts/math/SafeMath.sol";
 
@@ -10,20 +11,28 @@ abstract contract KittyCore is IERC721 {
     function transfer(address _to, uint256 _tokenId) external virtual;
 }
 
-contract CryptoKittiesBasket is Initializable {
-    // ckAddress = 0x16baF0dE678E52367adC69fD067E5eDd1D33e3bF;
+contract CryptoKittiesShop is Initializable {
     KittyCore _ck;
+    IERC20 _ourToken;
 
-    function initialize(KittyCore ck) public initializer {
+    function initialize(KittyCore ck, IERC20 ourToken) public initializer {
         //todo: check wether KittyCore.name() == "CryptoKitties")
         _ck = ck;
+        _ourToken = ourToken;
     }
 
     function giveMeKittie(uint256 tokenId) public {
+        uint256 allowance = _ourToken.allowance(msg.sender, address(this));
+        require(allowance >= 1);
+        _ourToken.transferFrom(msg.sender, address(this), 1);
         _ck.transfer(msg.sender, tokenId);
     }
 
     function getKittieBalance() public view returns (uint256) {
         return _ck.balanceOf(address(this));
+    }
+
+    function getOurTokenBalance() public view returns (uint256) {
+        return _ourToken.balanceOf(address(this));
     }
 }
