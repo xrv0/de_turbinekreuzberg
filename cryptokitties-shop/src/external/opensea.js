9e101934dab9c095a9e4f3bb60b3cf73da5573f3
xrv0 <33809091+xrv0@users.noreply.github.com>
2020-10-08 12:05:40 +0200

    Update opensea.js
    Fixed bug where an empty array would be returned

--- cryptokitties-shop/src/external/opensea.js
+++ cryptokitties-shop/src/external/opensea.js
@@ -3,7 +3,8 @@
  * @param {string} owner 
  */
 export async function retrieveContractAssets(erc721, owner) {
-    const url = `https://rinkeby-api.opensea.io/api/v1/assets/?asset_contract_addresses=${erc721}&owner=${owner}`;
+    let url = `https://rinkeby-api.opensea.io/api/v1/assets/?asset_contract_addresses=${erc721}&owner=${owner}`;
+    url = url.toLowerCase();
     const response = await fetch(url);
     const data = await response.json();
     return data.assets;
