973a6f53a450119d5c0a61a425c064644fefc663
xrv0 <33809091+xrv0@users.noreply.github.com>
2020-10-09 09:17:38 +0200

    Added rinkeby address

--- cryptokitties-shop/src/frontend/TokenList.js
+++ cryptokitties-shop/src/frontend/TokenList.js
@@ -21,7 +21,7 @@ export function renderTokenList(tokens, list, methodToCall, buttonText) {
                 <div class="kitty-offer-name">
                   <strong># ${token}</strong>
                 </div>
-                <img src="https://img.cryptokitties.co/0x06012c8cf97bead5deae237070f9587f8e7a266d/1671786.svg" alt="">
+                <img src="https://img.cryptokitties.co/0x06012c8cf97bead5deae237070f9587f8e7a266d/1671786.svg" alt="" class="kitty-offer-img">
                 <div class="kitty-offer-buy-button-outer">
                   <button class="kitty-offer-buy-button" onclick="CKShop.${methodToCall}(${token})">${buttonText}</button>
                 </div>
@@ -41,15 +41,15 @@ export function renderAssets(assets, list, methodToCall, buttonText) {
     }else {
         assets.map(asset => {
             const assetHtml = `
-        <div class="kitty-offer-outer">
-            <div class="kitty-offer-name">
-              <strong># ${asset.id}</strong>
+            <div class="kitty-offer-outer" style="background-color: ${asset.background_color}">
+                <div class="kitty-offer-name">
+                  <strong># ${asset.id}</strong>
+                </div>
+                <img src="${asset.image_original_url}" alt="" class="kitty-offer-img">
+                <div class="kitty-offer-buy-button-outer">
+                  <button class="kitty-offer-buy-button" onclick="CKShop.${methodToCall}(${asset.id})">${buttonText}</button>
+                </div>
             </div>
-            <img src="${asset.image_original_url}" alt="">
-            <div class="kitty-offer-buy-button-outer">
-              <button class="kitty-offer-buy-button" onclick="CKShop.${methodToCall}(${asset.id})">${buttonText}</button>
-            </div>
-        </div>
         `;
 
             const li = document.createElement("li");
